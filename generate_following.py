ff6c8f09001a33216cbec3f1e026dde8d3fc6d23
neeleshdodda44 <ndodda@berkeley.edu>
2019-08-28 16:18:37 -0700

    model updates

--- generate_following.py
+++ generate_following.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from keras_gpt_2 import load_trained_model_from_checkpoint, get_bpe_from_files, generate
+import tensorflow as tf
+import numpy as np
 
 
 if len(sys.argv) != 2:
@@ -16,12 +18,87 @@ vocab_path = os.path.join(model_folder, 'vocab.bpe')
 
 
 print('Load model from checkpoint...')
-model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
+#model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
 print('Load BPE from files...')
 bpe = get_bpe_from_files(encoder_path, vocab_path)
 print('Generate text...')
-output = generate(model, bpe, ['From the day forth, my arm'], length=20, top_k=40)
+# output = generate(model, bpe, ['From the day forth, my arm'], length=20, top_k=40)
+from itertools import chain
+
+# Let's define our contexts and special tokens
+persona = [["i", "like", "playing", "football", "."],
+           ["i", "am", "from", "NYC", "."]]
+history = [["hello", "how", "are", "you", "?"],
+           ["i", "am", "fine", "thanks", "."]]
+reply = ["great", "to", "hear"]
+speaker1, speaker2 = "You said: ", "I said: "
+
+def build_inputs(persona, history, reply):
+    # Build our sequence by adding delimiters and concatenating
+    sequence = [list(chain(*persona))] + history + [reply]
+    sequence = [sequence[0]] + [ [speaker2 if (len(sequence)-i) % 2 else speaker1] + s
+                                for i, s in enumerate(sequence[1:])]
+    # Build our word, segments and position inputs from the sequence
+    words = list(chain(*sequence))                          # word tokens
+    segments = [speaker2 if i % 2 else speaker1             # segment tokens
+                for i, s in enumerate(sequence) for _ in s]
+    position = list(range(len(words)))                      # position tokens
+    return words, segments, position, sequence
+
+words, segments, position, sequence = build_inputs(persona, history, reply)
+
+# >>> print(sequence)  # Our inputs looks like this:
+# [['<bos>', 'i', 'like', 'playing', 'football', '.', 'i', 'am', 'from', 'NYC', '.'],
+#  ['<speaker1>', 'hello', 'how', 'are', 'you', '?'],
+#  ['<speaker2>', 'i', 'am', 'fine', 'thanks', '.'],
+#  ['<speaker1>', 'great', 'to', 'hear', '<eos>']]
+
+# Tokenize words and segments embeddings:
+words = [bpe.encode(word) for word in words]
+segments = [bpe.encode(segment) for segment in segments]
 
 # If you are using the 117M model and top_k equals to 1, then the result would be:
 # "From the day forth, my arm was broken, and I was in a state of pain. I was in a state of pain,"
-print(output[0])
+
+
+# Let's add a distractor to our previously defined persona, history and reply
+distractor = ["sorry", "to", "hear", "that"]
+
+# Build & tokenize inputs ending with our distractor like we did with the gold reply
+words_distractor, segments_distractor, _, _ = build_inputs(persona, history, distractor)
+words_distractor = [bpe.encode(word) for word in words_distractor]
+segments_distractor = [bpe.encode(segment) for segment in segments_distractor]
+
+# Prepare our language modeling targets: keep only the reply segment, -1 on the rest
+lm_targets = ([-1] * sum(len(s) for s in sequence[:-1])) \
+             + [-1] + [bpe.encode(word) for word in sequence[-1][1:]]
+lm_distractor = [-1] * len(words_distractor)
+
+# Store the position of the last tokens for the next-sentence prediction loss
+last_token = len(words) - 1
+last_token_distractor = len(words_distractor) - 1
+
+# Now we can pad reply and distractor inputs and targets to the same length
+padding_length = max(len(words), len(words_distractor))
+def pad(x, padding):
+    return x + [padding] * (padding_length - len(x))
+
+(words, words_distractor,
+ segments, segments_distractor) = [pad(x, bpe.encode('<pad>'))
+                                   for x in (words, words_distractor,
+                                             segments, segments_distractor)]
+
+(lm_targets, lm_distractor) = [pad(x, -1) for x in (lm_targets, lm_distractor)]
+ 
+# And gather reply and distractor inputs to build the input tensors:
+# words tokens
+input_ids = np.array([[words, words_distractor]])
+# segment tokens
+token_type_ids = np.array([[segments, segments_distractor]])
+# Positions tokens can be automatically created by the model as (0, 1, ..., N)
+# Last tokens location
+mc_token_ids = np.array([[last_token, last_token_distractor]])
+# Language modeling labels
+lm_labels = np.array([[lm_targets, lm_distractor]])
+# Next-sentence prediction labels
+mc_labels = np.array([0])  # Gold reply is 1st (index 0)
--- keras_gpt_2/bpe.py
+++ keras_gpt_2/bpe.py
@@ -4,6 +4,10 @@ import json
 import codecs
 import regex as re
 
+import os
+import six
+from io import open
+
 
 __all__ = ['BytePairEncoding', 'get_bpe_from_files']
 
@@ -31,6 +35,7 @@ class BytePairEncoding(object):
         self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
         self.token_pattern = re.compile(r"'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+")
         self.cache = {}
+        self.unk_token = None
 
     @staticmethod
     def init_byte_encoder():
@@ -84,6 +89,104 @@ class BytePairEncoding(object):
     def decode(self, tokens):
         text = ''.join([self.token_dict_inv[token] for token in tokens])
         return bytearray([self.byte_decoder[byte] for byte in text]).decode('utf-8', errors='replace')
+
+
+    def add_tokens(self, new_tokens):
+        """
+        Add a list of new tokens to the tokenizer class. If the new tokens are not in the
+        vocabulary, they are added to it with indices starting from length of the current vocabulary.
+        Args:
+            new_tokens: list of string. Each string is a token to add. Tokens are only added if they are not already in the vocabulary (tested by checking if the tokenizer assign the index of the ``unk_token`` to them).
+        Returns:
+            Number of tokens added to the vocabulary.
+        Examples::
+            # Let's see how to increase the vocabulary of Bert model and tokenizer
+            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            model = BertModel.from_pretrained('bert-base-uncased')
+            num_added_toks = tokenizer.add_tokens(['new_tok1', 'my_new-tok2'])
+            print('We have added', num_added_toks, 'tokens')
+            model.resize_token_embeddings(len(tokenizer))  # Notice: resize_token_embeddings expect to receive the full size of the new vocabulary, i.e. the length of the tokenizer.
+        """
+        if not new_tokens:
+            return 0
+
+        to_add_tokens = []
+        for token in new_tokens:
+            assert isinstance(token, str) or (six.PY2 and isinstance(token, unicode))
+            if token != self.unk_token and \
+                    self.convert_tokens_to_ids(token) == self.convert_tokens_to_ids(self.unk_token):
+                to_add_tokens.append(token)
+                logger.info("Adding %s to the vocabulary", token)
+
+        added_tok_encoder = dict((tok, len(self) + i) for i, tok in enumerate(to_add_tokens))
+        added_tok_decoder = {v:k for k, v in added_tok_encoder.items()}
+        self.token_dict.update(added_tok_encoder)
+        self.token_dict_inv.update(added_tok_decoder)
+
+        return len(to_add_tokens)
+
+    def add_special_tokens(self, special_tokens_dict):
+        """
+        Add a dictionary of special tokens (eos, pad, cls...) to the encoder and link them
+        to class attributes. If special tokens are NOT in the vocabulary, they are added
+        to it (indexed starting from the last index of the current vocabulary).
+        Args:
+            special_tokens_dict: dict of string. Keys should be in the list of predefined special attributes:
+                [``bos_token``, ``eos_token``, ``unk_token``, ``sep_token``, ``pad_token``, ``cls_token``, ``mask_token``,
+                ``additional_special_tokens``].
+                Tokens are only added if they are not already in the vocabulary (tested by checking if the tokenizer assign the index of the ``unk_token`` to them).
+        Returns:
+            Number of tokens added to the vocabulary.
+        Examples::
+            # Let's see how to add a new classification token to GPT-2
+            tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+            model = GPT2Model.from_pretrained('gpt2')
+            special_tokens_dict = {'cls_token': '<CLS>'}
+            num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
+            print('We have added', num_added_toks, 'tokens')
+            model.resize_token_embeddings(len(tokenizer))  # Notice: resize_token_embeddings expect to receive the full size of the new vocabulary, i.e. the length of the tokenizer.
+            assert tokenizer.cls_token == '<CLS>'
+        """
+        if not special_tokens_dict:
+            return 0
+
+        added_tokens = 0
+        for key, value in special_tokens_dict.items():
+            assert key in self.SPECIAL_TOKENS_ATTRIBUTES
+            if key == 'additional_special_tokens':
+                assert isinstance(value, (list, tuple)) and all(isinstance(t, str) or (six.PY2 and isinstance(t, unicode)) for t in value)
+                added_tokens += self.add_tokens(value)
+            else:
+                assert isinstance(value, str) or (six.PY2 and isinstance(value, unicode))
+                added_tokens += self.add_tokens([value])
+            logger.info("Assigning %s to the %s key of the tokenizer", value, key)
+            setattr(self, key, value)
+
+        return added_tokens
+
+    def convert_tokens_to_ids(self, tokens):
+        """ Converts a single token, or a sequence of tokens, (str/unicode) in a single integer id
+            (resp. a sequence of ids), using the vocabulary.
+        """
+        if isinstance(tokens, str) or (six.PY2 and isinstance(tokens, unicode)):
+            return self._convert_token_to_id_with_added_voc(tokens)
+
+        ids = []
+        for token in tokens:
+            ids.append(self._convert_token_to_id_with_added_voc(token))
+        if len(ids) > self.max_len:
+            logger.warning("Token indices sequence length is longer than the specified maximum sequence length "
+                           "for this model ({} > {}). Running this sequence through the model will result in "
+                           "indexing errors".format(len(ids), self.max_len))
+        return ids
+
+    def _convert_token_to_id_with_added_voc(self, token):
+        if token in self.token_dict:
+            return self.token_dict[token]
+        return self._convert_token_to_id(token)
+
+    def _convert_token_to_id(self, token):
+        raise NotImplementedError
 
 
 def get_bpe_from_files(encoder_path, vocab_path):
