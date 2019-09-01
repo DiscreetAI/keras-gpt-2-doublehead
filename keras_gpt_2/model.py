6b608b5378fbb061d37f913dea312919caf26cd4
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-01 15:07:10 -0700

    finished gpt2doubleheadsmodel + weights

--- keras_gpt_2/model.py
+++ keras_gpt_2/model.py
@@ -6,7 +6,6 @@ from keras_transformer import gelu, attention_builder, feed_forward_builder
 from keras_transformer import get_custom_objects as get_transformer_custom_objects
 
 from keras.layers import Dense, Layer, Dropout
-from keras.utils import one
 from keras import backend as K
 from .sequence_summary import SequenceSummary
 
