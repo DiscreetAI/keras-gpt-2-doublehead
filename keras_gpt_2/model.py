4c004a8b26431618c646d3647cebdb9e23085413
neeleshdodda44 <ndodda@berkeley.edu>
2019-08-31 18:43:45 -0700

    finished gpt2doubleheadsmodel + weights

--- keras_gpt_2/model.py
+++ keras_gpt_2/model.py
@@ -147,18 +147,8 @@ def get_model(n_vocab,
 
     mc_sequence_summary = SequenceSummary(
         name='MCSequenceSummary'
-    )([norm_layer, mc_input_layer])
+    )(norm_layer)
 
-    mc_linear = Dense(
-        units=1,
-        input_shape=(n_embd,),
-        name='MCDense'
-    )(mc_sequence_summary)
-
-    mc_head = Dropout(
-        rate=0.1,
-        name='MCOutput'
-    )(mc_linear)
 
 
 
--- keras_gpt_2/sequence_summary.py
+++ keras_gpt_2/sequence_summary.py
@@ -7,7 +7,7 @@ class SequenceSummary(Layer):
     def __init__(self, name):
         super(SequenceSummary, self).__init__(name=name)
 
-        self.summary_type = 'cls_index'
+        self.summary_type = 'last'
         # self.summary = Identity()
         # if hasattr(config, 'summary_use_proj') and config.summary_use_proj:
         #     if hasattr(config, 'summary_proj_to_labels') and config.summary_proj_to_labels and config.num_labels > 0:
@@ -35,7 +35,7 @@ class SequenceSummary(Layer):
                 if summary_type == 'cls_index' and cls_index is None:
                     we take the last token of the sequence as classification token
         """
-        hidden_states, cls_index = inputs
+        hidden_states = inputs
         print(K.int_shape(hidden_states), "ZERO")
         print(hidden_states, cls_index)
         print(K.int_shape(cls_index), "ONE")
