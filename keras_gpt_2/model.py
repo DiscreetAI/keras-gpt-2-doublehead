ad508e7fb5a3a88de7de331ad683ac047d21c8b6
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-02 15:21:12 -0700

    debugging metric functions

--- keras_gpt_2/model.py
+++ keras_gpt_2/model.py
@@ -214,7 +214,7 @@ def cross_entropy(logits, labels, ignore_index=None):
     return xentropy
     
 def mc_loss_function(mc_labels, mc_logits):
-    mc_labels, dim = tf.split(axis=0, value=mc_labels, num_split=2)
+    mc_labels, dim = tf.split(0, mc_labels, 2)
     print(dim)
     mc_loss = cross_entropy( 
         K.reshape(mc_logits, (-1, K.int_shape(mc_logits)[-1])),
