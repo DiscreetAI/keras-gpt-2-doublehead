091c1f8b291f2ac3de266d4f1e1e7d0f87fceee7
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-02 14:47:22 -0700

    debugging metric functions

--- keras_gpt_2/metrics.py
+++ keras_gpt_2/metrics.py
@@ -99,27 +99,27 @@ def top_3_mc(y_true, y_pred):
 def precision_lm(y_true, y_pred):
     y_true = tf.cast(y_true, tf.int64)
     # y_true = K.reshape(tf.cast(one_hot(y_true, 50257, axis=-1), tf.float32), (-1, 50257))
-    print(y_pred.shape)
+    
     y_pred = K.argmax(y_pred, axis=-1)
-    print(y_pred.shape)
+    
     y_true = K.reshape(y_true, (1, -1))
-    print(y_true.shape)
+    
     return precision_m(y_true, y_pred)
 
 def precision_mc(y_true, y_pred):
     y_true = tf.cast(y_true, tf.int64)
+    print(y_pred.shape)
     y_pred = K.argmax(y_pred, axis=-1)
+    print(y_pred.shape)
     y_true = K.flatten(y_true)
+    print(y_true.shape)
     return precision_m(y_true, y_pred)
 
 def f1_score_lm(y_true, y_pred):
     y_true = tf.cast(y_true, tf.int64)
     # y_true = K.reshape(tf.cast(one_hot(y_true, 50257, axis=-1), tf.float32), (-1, 50257))
-    print(y_pred.shape)
     y_pred = K.argmax(y_pred, axis=-1)
-    print(y_pred.shape)
     y_true = K.reshape(y_true, (1, -1))
-    print(y_true.shape)
     return f1_m(y_true, y_pred)
 
 def f1_score_mc(y_true, y_pred):
