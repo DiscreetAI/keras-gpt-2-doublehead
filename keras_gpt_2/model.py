adaed37ccaa3239af1a3e7c16ec58aa80c6e4c1e
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-02 00:10:55 -0700

    debugging metric functions

--- keras_gpt_2/model.py
+++ keras_gpt_2/model.py
@@ -185,7 +185,7 @@ def get_model(n_vocab,
         optimizer=keras.optimizers.Adam(),
         loss=losses,
         loss_weights=lossWeights,
-        metrics=metrics
+        #metrics=metrics
     )
     return model
 
