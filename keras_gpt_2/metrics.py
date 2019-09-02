00f156e38a1e5b6f8e9f61d3473d7905082f0908
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-01 23:17:52 -0700

    debugging metric functions

--- keras_gpt_2/metrics.py
+++ keras_gpt_2/metrics.py
@@ -39,9 +39,9 @@ def f1_m(y_true, y_pred):
 
 
 def perplexity_lm(y_true, y_pred):
+    y_true = tf.cast(y_true, tf.int32)
+    y_true = K.reshape(tf.cast(one_hot(y_true, 50257, axis=-1), tf.float32), (-1, 50257))
     print(y_true.shape, y_pred.shape, "HI")
-    # y_true = tf.cast(y_true, tf.int32)
-    # y_true = K.reshape(tf.cast(one_hot(y_true, 50257, axis=-1), tf.float32), (-1, 50257))
     return perplexity(y_true, y_pred)
 
 def perplexity_mc(y_true, y_pred):
