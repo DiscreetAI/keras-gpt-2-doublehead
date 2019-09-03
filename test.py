d616f9c64b19912cbbca14ef5a372764db5908ad
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-03 14:58:48 -0700

    adding tf federated

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 import numpy as np
 from collections import defaultdict
 import os
+from keras_gpt_2 import load_trained_model_from_checkpoint, get_bpe_from_files, generate
+
 
 model_folder = 'models/117M'
 config_path = os.path.join(model_folder, 'hparams.json')
