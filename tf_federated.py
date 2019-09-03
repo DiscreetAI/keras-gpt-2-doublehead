619b0d916c3b5fb8ebfb4bcd92988dede5ad6463
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-03 16:56:29 -0700

    adding tf federated

--- tf_federated.py
+++ tf_federated.py
@@ -37,91 +37,67 @@ print(input_ids.shape)
 print(mc_token_ids.shape)
 print(mc_labels.shape)
 
-model_folder = 'models/117M'
-config_path = os.path.join(model_folder, 'hparams.json')
-checkpoint_path = os.path.join(model_folder, 'model.ckpt')
-encoder_path = os.path.join(model_folder, 'encoder.json')
-vocab_path = os.path.join(model_folder, 'vocab.bpe')
+num_clients = 5
+
+batches = [np.array_split(input_ids, num_clients), np.array_split(lm_labels, num_clients), np.array_split(mc_token_ids, num_clients), np.array_split(mc_labels, num_clients)]
+
+assert len(batches) == 4
+assert len(batches[0]) == num_clients
+
+datasets = list(zip(*batches))
+
+assert len(datasets) == num_clients
+assert len(datasets[0]) == 4
+
+a, b, c, d = datasets[0]
+print(a.shape, b.shape, c.shape, d.shape)
+tf_datasets = []
+# for input_ids, lm_labels, mc_token_ids, mc_labels in datasets:
+#     tf_datasets.append()
+
+def dataset_map(input_ids, lm_labels, mc_token_ids, mc_labels):
+    return {
+        'x': {
+            'LMInput': K.expand_dims(input_ids),
+            'MCInput': K.expand_dims(mc_token_ids)
+        },
+        'y': {
+            'LMOutput': K.expand_dims(lm_labels),
+            'MCOutput': K.expand_dims(mc_labels)
+        }
+    }
+
+#datasets = [tuple(dataset) for dataset in datasets]
+datasets = [tf.data.Dataset.from_tensor_slices(dataset) for dataset in datasets]
+datasets = [dataset.map(dataset_map) for dataset in datasets]
+
+train_data = datasets
+
+# Grab a single batch of data so that TFF knows what data looks like.
+sample_batch = tf.nest.map_structure(
+    lambda x: x.numpy(), iter(train_data[0]).next())
+
+print(sample_batch)
 
 
-print('Load model from checkpoint...')
-model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
+def model_fn():
+    model_folder = 'models/117M'
+    config_path = os.path.join(model_folder, 'hparams.json')
+    checkpoint_path = os.path.join(model_folder, 'model.ckpt')
+    encoder_path = os.path.join(model_folder, 'encoder.json')
+    vocab_path = os.path.join(model_folder, 'vocab.bpe')
 
-model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
-history_output = model.fit(
-    {
-        'LMInput': input_ids,
-        'MCInput': mc_token_ids
-    },
-    {
-        'LMOutput': lm_labels,
-        'MCOutput': mc_labels
-    },
-    batch_size=1,
-    epochs=3,
-    callbacks=[BaseLogger()]
-)
-# num_clients = 5
+    if not os.path.isdir(model_folder):
+        gpt2.download_gpt2(model_name = '117M')
 
-# batches = [np.array_split(input_ids, num_clients), np.array_split(lm_labels, num_clients), np.array_split(mc_token_ids, num_clients), np.array_split(mc_labels, num_clients)]
+    print('Load BPE from files...')
+    bpe = get_bpe_from_files(encoder_path, vocab_path)
+    model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
+    return tff.learning.from_compiled_keras_model(model, sample_batch)
 
-# assert len(batches) == 4
-# assert len(batches[0]) == num_clients
-
-# datasets = list(zip(*batches))
-
-# assert len(datasets) == num_clients
-# assert len(datasets[0]) == 4
-
-# a, b, c, d = datasets[0]
-# print(a.shape, b.shape, c.shape, d.shape)
-# tf_datasets = []
-# # for input_ids, lm_labels, mc_token_ids, mc_labels in datasets:
-# #     tf_datasets.append()
-
-# def dataset_map(input_ids, lm_labels, mc_token_ids, mc_labels):
-#     return {
-#         'x': {
-#             'LMInput': K.expand_dims(input_ids),
-#             'MCInput': K.expand_dims(mc_token_ids)
-#         },
-#         'y': {
-#             'LMOutput': K.expand_dims(lm_labels),
-#             'MCOutput': K.expand_dims(mc_labels)
-#         }
-#     }
-
-# #datasets = [tuple(dataset) for dataset in datasets]
-# datasets = [tf.data.Dataset.from_tensor_slices(dataset) for dataset in datasets]
-# datasets = [dataset.map(dataset_map) for dataset in datasets]
-
-# train_data = datasets
-
-# # Grab a single batch of data so that TFF knows what data looks like.
-# sample_batch = tf.nest.map_structure(
-#     lambda x: x.numpy(), iter(train_data[0]).next())
-
-# print(sample_batch)
-
-
-# def model_fn():
-#     model_folder = 'models/117M'
-#     config_path = os.path.join(model_folder, 'hparams.json')
-#     checkpoint_path = os.path.join(model_folder, 'model.ckpt')
-#     encoder_path = os.path.join(model_folder, 'encoder.json')
-#     vocab_path = os.path.join(model_folder, 'vocab.bpe')
-
-#     if not os.path.isdir(model_folder):
-#         gpt2.download_gpt2(model_name = '117M')
-
-#     print('Load BPE from files...')
-#     bpe = get_bpe_from_files(encoder_path, vocab_path)
-#     model = load_trained_model_from_checkpoint(config_path, checkpoint_path)
-#     return tff.learning.from_compiled_keras_model(model, sample_batch)
-
-# # Simulate a few rounds of training with the selected client devices.
-# trainer = tff.learning.build_federated_averaging_process(model_fn)
-# state = trainer.initialize()
-# for _ in range(5):
-#   state, metrics = trainer.next(state, train_data)
-#   print (metrics.loss)
+# Simulate a few rounds of training with the selected client devices.
+trainer = tff.learning.build_federated_averaging_process(model_fn)
+state = trainer.initialize()
+for _ in range(5):
+  state, metrics = trainer.next(state, train_data)
+  print (metrics.loss)
