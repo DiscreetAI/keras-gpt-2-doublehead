6e4212e51dc11a591e72b6eb68b9a8d26dddea81
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-03 15:02:43 -0700

    adding tf federated

--- keras_gpt_2/model.py
+++ keras_gpt_2/model.py
@@ -1,4 +1,4 @@
-from .backend import keras
+from .backend import keras as keras
 from keras_embed_sim import EmbeddingRet, EmbeddingSim
 from keras_pos_embd import PositionEmbedding
 from keras_layer_normalization import LayerNormalization
