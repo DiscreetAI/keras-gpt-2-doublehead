6449c06cbc68f5a5a9227cba994faa4fc672a92b
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-03 23:44:33 -0700

    testing one head

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ import os
 from keras_gpt_2 import load_trained_model_from_checkpoint, get_bpe_from_files, generate
 import requests
 
+from tensorflow.python.client import device_lib
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+print(get_available_devices()) 
+
 model_folder = 'models/117M'
 config_path = os.path.join(model_folder, 'hparams.json')
 checkpoint_path = os.path.join(model_folder, 'model.ckpt')
