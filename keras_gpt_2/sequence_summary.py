44a1e4f2286d1b28ef4fdeb095f403a97496895e
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-04 16:01:07 -0700

    fixing metrics

--- keras_gpt_2/sequence_summary.py
+++ keras_gpt_2/sequence_summary.py
@@ -52,7 +52,8 @@ class SequenceSummary(Layer):
                 cls_index = K.tile(cls_index, args)
                 print(cls_index.shape, "CLS_INDEX")
             # shape of cls_index: (bsz, XX, 1, hidden_size) where XX are optional leading dim of hidden_states
-            output = K.squeeze(tf.gather(params=hidden_states, indices=tf.cast(cls_index, tf.int32), axis=-1), -2) # shape (bsz, XX, hidden_size)
+            gather = 
+            output = K.squeeze(tf.gather(params=hidden_states, indices=tf.cast(cls_index, tf.int32), axis=-2), -2) # shape (bsz, XX, hidden_size)
             print(output.shape, "SEQUENCE")
         elif self.summary_type == 'attn':
             raise NotImplementedError
