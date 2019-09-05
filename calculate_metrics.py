438a25e7fbd1c091ababddef9b8c0034ae2cdf78
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-05 16:56:07 -0700

    calculating metrics on validation set

---
+++ calculate_metrics.py
@@ -0,0 +1,92 @@
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import BaseLogger, History
+import tensorflow as tf
+import numpy as np
+from collections import defaultdict
+from keras_gpt_2 import load_trained_model_from_checkpoint, get_bpe_from_files, generate
+import requests
+import gpt_2_simple as gpt2
+from keras_gpt_2 import perplexity_lm, f1_score_lm, top_1_mc
+
+from tensorflow.python.client import device_lib
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+print(get_available_devices()) 
+
+model_folder = 'models/117M'
+config_path = os.path.join(model_folder, 'hparams.json')
+checkpoint_path = os.path.join(model_folder, 'model.ckpt')
+encoder_path = os.path.join(model_folder, 'encoder.json')
+vocab_path = os.path.join(model_folder, 'vocab.bpe')
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+filenames = ['valid_input_ids.json', 'valid_lm_labels.json', 'valid_mc_labels.json', 'valid_mc_token_ids.json']
+
+url = "https://persona-dataset.s3.amazonaws.com/{}"
+
+data = []
+
+for name in filenames:
+    full_url = url.format(name)
+    json_data = requests.get(full_url).json()
+    data.append(np.array(json_data))
+    print("Done")
+
+input_ids, lm_labels, mc_labels, mc_token_ids = data
+
+print(lm_labels.shape)
+print(input_ids.shape)
+
+print(mc_token_ids.shape)
+print(mc_labels.shape)
+
+# index = (131438 // 16) * 16
+# print(index) 
+
+# input_ids = input_ids[:index]
+# lm_labels = lm_labels[:index]
+# mc_labels = mc_labels[:index]
+# mc_token_ids = mc_token_ids[:index]
+
+if not os.path.isdir(model_folder):
+    gpt2.download_gpt2(model_name = '117M')
+
+model = load_trained_model_from_checkpoint(config_path, checkpoint_path, batch_size=batch_size)
+
+lm_logits, mc_logits = model.predict([input_ids, mc_token_ids])
+
+ppl = perplexity_lm(lm_labels, lm_logits)
+f1 = f1_score_lm(lm_labels, lm_logits)
+top_1 = top_1_mc(mc_labels, mc_logits)
+
+print("Perplexity", ppl)
+print("F1 Score", f1)
+print("Hits@1", top_1)
+
+# strategy = tf.distribute.MirroredStrategy()
+
+# print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+# batch_size = 2
+# with strategy.scope():
+    
+#     print("starting fit")
+#     history_output = model.fit(
+#         {
+#             'LMInput': input_ids,
+#             'MCInput': mc_token_ids
+#         },
+#         {
+#             'LMOutput': lm_labels,
+#             'MCOutput': mc_labels
+#         },
+#         batch_size=batch_size * strategy.num_replicas_in_sync,
+#         epochs=2,
+#         callbacks=[tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+#                                        save_weights_only=True)]
+#     )
+#     # import json
+
+#     # with open('training_history.json', 'w') as f:
+#     #     json.dump(history_output.history, f)
