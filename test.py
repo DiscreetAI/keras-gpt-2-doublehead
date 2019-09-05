bb83a76563a20e809b8832a2a97236bee7390d27
neeleshdodda44 <ndodda@berkeley.edu>
2019-09-04 17:45:11 -0700

    fixing metrics

--- test.py
+++ test.py
@@ -57,7 +57,7 @@ with strategy.scope():
         },
         batch_size=batch_size * strategy.num_replicas_in_sync,
         epochs=3,
-        callbacks=[
+        callbacks=[Metrics(),
             tf.keras.callbacks.TensorBoard(log_dir='./logs'),
             tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
                                        save_weights_only=True)]
